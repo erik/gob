1eb2725f5897b7b0be0b25110998c8a0f9d302a3
Erik Price <erik.price16@gmail.com>
2013-01-10 16:56:28 -0500

    Add some error handling capability to parser

--- parser.go
+++ parser.go
@@ -1,11 +1,27 @@
 package main
 
 import (
+	"fmt"
 	"io"
 )
 
+type ParseError struct {
+	tok Token
+	msg string
+}
+
+func (p *ParseError) Error() string {
+	return fmt.Sprintf("Parse error on line %d, at token: %s: %s",
+		p.tok.start.Line, p.tok.String(), p.msg)
+}
+
+func NewParseError(tok Token, msg string) error {
+	return &ParseError{tok, msg}
+}
+
 type Parser struct {
-	lex *Lexer
+	lex   *Lexer
+	token Token
 }
 
 func NewParser(name string, input io.Reader) *Parser {
@@ -16,8 +32,20 @@ func NewParser(name string, input io.Reader) *Parser {
 	return parse
 }
 
-func (p *Parser) Parse() {
+func (p *Parser) Parse() error {
+	tok, _ := p.lex.NextToken()
+	return NewParseError(tok, "Parser not implemented")
+}
 
+func (p *Parser) nextToken() (Token, error) {
+	tok, err := p.lex.NextToken()
+
+	if err != nil {
+		return tok, err
+	}
+
+	p.token = tok
+	return tok, nil
 }
 
 func (p *Parser) expect() {
