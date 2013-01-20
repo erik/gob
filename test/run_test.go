a75ef343ef02d72f787a6a27a7c850bcf9904bbf
Erik Price <erik.price16@gmail.com>
2013-01-19 23:06:36 -0500

    Add some sample programs, commented out test runner

---
+++ test/run_test.go
@@ -0,0 +1,26 @@
+package test
+
+import (
+	_ "github.com/boredomist/gob/parse"
+	_ "os"
+	"testing"
+)
+
+var tests = []string{"convert.b", "copy.b", "lower.b", "snide.b"}
+
+func TestExampleDummy(t *testing.T) {
+}
+
+// TODO: parser is not quite ready for this, and I'd rather not have every test fail
+// func TestExamples(t *testing.T) {
+// 	for _, test := range tests {
+// 		if file, err := os.Open(test); err != nil {
+// 			t.Errorf("failed to open test: %s", err)
+// 		} else {
+// 			p := parse.NewParser(test, file)
+// 			if _, err := p.Parse(); err != nil {
+// 				t.Errorf("%s failed: %v", test, err)
+// 			}
+// 		}
+// 	}
+// }
