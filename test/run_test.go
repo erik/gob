046643d24883f03da737becfd0b91df144180363
Erik Price <erik.price16@gmail.com>
2013-01-20 15:19:33 -0500

    Uncomment (currently failing) tests

--- test/run_test.go
+++ test/run_test.go
@@ -1,8 +1,8 @@
 package test
 
 import (
-	_ "github.com/boredomist/gob/parse"
-	_ "os"
+	"github.com/boredomist/gob/parse"
+	"os"
 	"testing"
 )
 
@@ -12,15 +12,15 @@ func TestExampleDummy(t *testing.T) {
 }
 
 // TODO: parser is not quite ready for this, and I'd rather not have every test fail
-// func TestExamples(t *testing.T) {
-// 	for _, test := range tests {
-// 		if file, err := os.Open(test); err != nil {
-// 			t.Errorf("failed to open test: %s", err)
-// 		} else {
-// 			p := parse.NewParser(test, file)
-// 			if _, err := p.Parse(); err != nil {
-// 				t.Errorf("%s failed: %v", test, err)
-// 			}
-// 		}
-// 	}
-// }
+func TestExamples(t *testing.T) {
+	for _, test := range tests {
+		if file, err := os.Open(test); err != nil {
+			t.Errorf("failed to open test: %s", err)
+		} else {
+			p := parse.NewParser(test, file)
+			if _, err := p.Parse(); err != nil {
+				t.Errorf("%s failed: %v", test, err)
+			}
+		}
+	}
+}
