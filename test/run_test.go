a27e71709004c0aea45e691bc828eb10bc1bee07
Erik Price <erik.price16@gmail.com>
2013-01-31 15:39:08 -0600

    Remove dummy test

--- test/run_test.go
+++ test/run_test.go
@@ -8,10 +8,6 @@ import (
 
 var tests = []string{"convert.b", "copy.b", "lower.b", "snide.b"}
 
-func TestExampleDummy(t *testing.T) {
-}
-
-// TODO: parser is not quite ready for this, and I'd rather not have every test fail
 func TestExamples(t *testing.T) {
 	for _, test := range tests {
 		if file, err := os.Open(test); err != nil {
