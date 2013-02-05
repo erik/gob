3f0d0607eb75e4e7a90a275b304b27e238604ede
Erik Price <erik.price16@gmail.com>
2013-02-05 14:55:39 -0600

    Check for duplicated variable/function names

--- parse/analyze.go
+++ parse/analyze.go
@@ -11,7 +11,7 @@ type SemanticError struct {
 }
 
 func (s *SemanticError) Error() string {
-	return fmt.Sprintf("Semantic error on %v: %v", s.node, s.msg)
+	return fmt.Sprintf("Semantic error on `%v`: %v", s.node, s.msg)
 }
 
 func NewSemanticError(node Node, msg string) error {
@@ -26,4 +26,43 @@ type TranslationUnit struct {
 
 func (t TranslationUnit) Verify() error {
 	return errors.New("Verification not implemented")
+}
+
+// TODO: resolve auto variable declarations within function definitions
+func (t TranslationUnit) ResolveDuplicates() error {
+	idents := map[string]Node{}
+
+	for _, fn := range t.funcs {
+		if _, ok := idents[fn.name]; ok {
+			return NewSemanticError(fn, "Duplicate function name")
+		}
+
+		idents[fn.name] = fn
+	}
+
+	for _, v := range t.vars {
+		var name string
+
+		switch v.(type) {
+		case ExternVecInitNode:
+			name = v.(ExternVecInitNode).name
+		case ExternVarInitNode:
+			name = v.(ExternVarInitNode).name
+		default:
+			return NewSemanticError(v, "Not variable init")
+		}
+
+		if _, ok := idents[name]; ok {
+			return NewSemanticError(v, "Duplicate variable name")
+		}
+
+		idents[name] = v
+	}
+
+	return nil
+}
+
+func (t TranslationUnit) ResolveExternalDeclarations() error {
+	// TODO: This should check that all declared extern vars resolve
+	return errors.New("Not yet implemented")
 }
--- parse/analyze_test.go
+++ parse/analyze_test.go
@@ -1,9 +1,37 @@
 package parse
 
 import (
+	"strings"
 	"testing"
 )
 
-// TODO: write these
-func TestVerifyDummy(_ *testing.T) {
+func TestAnalyzeDuplicate(t *testing.T) {
+	unit, err := NewParser("", strings.NewReader("a; b; c;")).Parse()
+
+	if err != nil {
+		t.Errorf("Parse failed: %v", err)
+	} else if err = unit.ResolveDuplicates(); err != nil {
+		t.Errorf("Resolve duplicates: %v", err)
+	}
+
+	unit, err = NewParser("", strings.NewReader("a; b; a;")).Parse()
+	if err != nil {
+		t.Errorf("Parse failed: %v", err)
+	} else if err = unit.ResolveDuplicates(); err == nil {
+		t.Errorf("Allowed duplicate variable/variable")
+	}
+
+	unit, err = NewParser("", strings.NewReader("a(){} a(){}")).Parse()
+	if err != nil {
+		t.Errorf("Parse failed: %v", err)
+	} else if err = unit.ResolveDuplicates(); err == nil {
+		t.Errorf("Allowed duplicate func/func")
+	}
+
+	unit, err = NewParser("", strings.NewReader("a; a(){}")).Parse()
+	if err != nil {
+		t.Errorf("Parse failed: %v", err)
+	} else if err = unit.ResolveDuplicates(); err == nil {
+		t.Errorf("Allowed duplicate func/variable")
+	}
 }
