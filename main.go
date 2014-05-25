49df449a3b56a52f503eb61292c0bfc9b7151703
Erik Price <erik.price16@gmail.com>
2014-05-24 23:42:32 -0400

    Add --parse-only flag

--- main.go
+++ main.go
@@ -13,9 +13,9 @@ const GOB_VERSION = "0.0.0"
 var (
 	showVersion = opt.Flag([]string{"-v", "--version"}, []string{},
 		"Show version info", "")
+	parseOnly = opt.Flag([]string{"-p", "--parse-only"}, []string{},
+		"Don't output anything, just parse", "")
 	outFile = opt.Flag([]string{"-o"}, []string{}, "Specify output file", "")
-
-	// TODO: other
 )
 
 func main() {
@@ -32,6 +32,10 @@ func main() {
 	}
 
 	for _, name := range opt.Args {
+		if len(opt.Args) > 1 {
+			fmt.Printf("==== %s ====\n", name)
+		}
+
 		file, err := os.Open(name)
 		if err != nil {
 			fmt.Println(err)
@@ -43,16 +47,17 @@ func main() {
 		unit, err := parser.Parse()
 		if err != nil {
 			fmt.Println(err)
-		} else {
-			//fmt.Println(unit)
 		}
 
 		if err = unit.Verify(); err != nil {
 			fmt.Println(err)
 		}
 
+		if *parseOnly {
+			continue
+		}
+
 		var emit emit.CEmitter
 		fmt.Println(emit.Emit(unit))
-
 	}
 }
